c6f6eff6e2b9565dbb14ce4091f57ea86cf62e4c
Rehina Yakubiv <reginayakubiv@gmail.com>
2025-01-16 21:15:32 -0500

    Custom navbar element

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import react from "react";
+import { faHouse, faUser, faCog } from '@fortawesome/free-solid-svg-icons';
 import NavBarElement from "./NavBarElement";
+
 
 function NavBar(){
     return(
-        <>
-            <NavBarElement />
-        </>
+        <div className="nav-bar">
+            <NavBarElement text="Home" icon={faHouse}/>
+        </div>
     );
 }
 
--- client/src/components/NavBarElement.jsx
+++ client/src/components/NavBarElement.jsx
@@ -1,13 +1,12 @@
 import react from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import "../../public/styles/navBarElement.css";
 
-function NavBarComponent(){
+function NavBarComponent({ text, icon }){
     return(
         <button className='nav-element'>
-           <FontAwesomeIcon icon={faHouse} className="navbar-icon" />
-            Home
+           <FontAwesomeIcon icon={icon} className="navbar-icon" />
+            {text}
         </button>
     );
 }
