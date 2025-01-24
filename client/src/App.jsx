92ae83341d9d57a4a10a97aae34103b1dd104a11
Rehina Yakubiv <reginayakubiv@gmail.com>
2025-01-23 20:25:59 -0500

    Added profile + styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import Login from "./components/Login";
 import Home from './components/Home';
+import Profile from './components/Profile';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import './App.css';
 
 function App() {
   return (
     //<Login />
-    <Home />
+    //<Home />
+    <Profile />
   );
 }
 
